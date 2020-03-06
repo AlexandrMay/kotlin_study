1d4bb527fd1fc3be2e211cc0525b73f4f36beaf7
AlexandrMay <maysalexandr@gmail.com>
2020-03-06 15:07:01 +0200

    app ended and tested

--- app/src/main/java/com/may/dinnerdecider/MainActivity.kt
+++ app/src/main/java/com/may/dinnerdecider/MainActivity.kt
@@ -2,11 +2,36 @@ package com.may.dinnerdecider
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    val foodList = arrayListOf("Chinese", "Hamburger", "Pizza", "KFC", "Starbucks")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        decideButton.setOnClickListener {
+            val random = Random()
+            val randomFood = random.nextInt(foodList.count())
+            selectedFoodText.text = foodList[randomFood]
+        }
+
+        addFoodButton.setOnClickListener {
+            val newFood = addFoodText.text.toString()
+            if (newFood.equals("") || newFood.length > 10){
+                val text = "Enter any name from 1 to 10 symbols!"
+                val duration = Toast.LENGTH_SHORT
+                val toast = Toast.makeText(applicationContext, text, duration)
+                toast.show()
+            } else {
+                foodList.add(newFood)
+                addFoodText.text.clear()
+                println(foodList)
+            }
+        }
     }
 }
